89df3b3a33d661a27cd9ee0a95c94bdd9316b092
Burak <burak.yilmaz@simdatix.com>
2022-03-09 14:34:11 +0300

    add createContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, createContext, useEffect } from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
@@ -9,24 +9,51 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
+export const UserContext = createContext();
+
 const App = () => {
   // defining states
+  const [user, setUser] = useState({
+    email: "",
+    name: "",
+    jobTitle: "",
+  });
+
   // defining const variables
   // defining custom hooks
+
   // defining effects
+  useEffect(() => {
+    console.log("data fetching...");
+
+    // will be called after 2000 ms
+    // async
+    return setTimeout(() => {
+      const userData = {
+        email: "test@hotmail.com",
+        name: "John Kennen",
+        jobTitle: "Full Stack Developer",
+      };
+      setUser(userData);
+    }, 2000);
+  }, [])
+
   // defining functions
+
   return (
-    <>
-      <Header/>
-      <Nav/>
-      <About/>
-      <Experience/>
-      <Services/>
-      <Portfolio/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
-    </>
+    <UserContext.Provider value={{
+      user, setUser
+    }}>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </UserContext.Provider>
   )
 }
 
