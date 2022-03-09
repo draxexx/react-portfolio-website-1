7082bd78cd7b0e74436af58562bfbf02580ef7e6
Burak <burak.yilmaz@simdatix.com>
2022-03-09 15:24:06 +0300

    create About component and AboutCard component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
 import "./about.css";
+import ME from "../../assets/me.png"
+import AboutCard from "./AboutCard";
+import { FaAward } from "react-icons/fa"
+import { FiUsers } from "react-icons/fi"
+import { VscFolderLibrary } from "react-icons/vsc"
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <AboutCard title="Experience" description="3+ Years Working" icon={<FaAward className="about__icon" />} />
+            <AboutCard title="Clients" description="200+ Worldwide" icon={<FiUsers className="about__icon" />} />
+            <AboutCard title="Projects" description="80+ Completed" icon={<VscFolderLibrary className="about__icon" />} />
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium adipisci nostrum dolorem voluptatibus. Doloribus id reprehenderit vitae, dolor voluptate, modi repellendus nam, nisi veritatis odit eaque dolore expedita deserunt officiis.</p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/about/AboutCard.jsx
@@ -0,0 +1,12 @@
+
+const AboutCard = ({ title, description, icon }) => {
+    return (
+        <article className="about__card">
+            {icon}
+            <h5>{title}</h5>
+            <small>{description}</small>
+        </article>
+    )
+}
+
+export default AboutCard
