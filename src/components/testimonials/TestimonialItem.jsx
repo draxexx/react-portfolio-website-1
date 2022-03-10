0c5fd6213f9d68634fb7628d7af81155738f2855
Burak <burak.yilmaz@simdatix.com>
2022-03-10 10:43:52 +0300

    add CSS for the Testimonials Component

--- src/components/testimonials/TestimonialItem.jsx
+++ src/components/testimonials/TestimonialItem.jsx
@@ -1,13 +1,15 @@
+import "./testimonials.css";
+
 const TestimonialItem = ({ name, avatar }) => {
     return (
         <article className="testimonial">
             <div className="client__avatar">
                 <img src={avatar} alt="Avatar One" />
-                <h5 className="client__name">{name}</h5>
-                <small className="client__review">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iste illo ullam et quae excepturi, ea officia dolorum nemo quod, laboriosam blanditiis repudiandae. Beatae placeat minima ullam eos earum voluptas!
-                </small>
             </div>
+            <h5 className="client__name">{name}</h5>
+            <small className="client__review">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iste illo ullam et quae excepturi, ea officia dolorum nemo quod, laboriosam blanditiis repudiandae. Beatae placeat minima ullam eos earum voluptas!
+            </small>
         </article>
     )
 }
