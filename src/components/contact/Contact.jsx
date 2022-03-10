a3bad4bdbd8d3300707b13f6430c1ea81b006db2
Burak <burak.yilmaz@simdatix.com>
2022-03-10 11:42:00 +0300

    create Contact Component and ContactOption Component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import "./contact.css";
+import ContactOption from './ContactOption';
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <ContactOption icon={<MdOutlineEmail />} title="Email" content="test@gmail.com" href="mailto:test@gmail.com" />
+          <ContactOption icon={<RiMessengerLine />} title="Messenger" content="egator" href="https://messenger.com" />
+          <ContactOption icon={<BsWhatsapp />} title="WhatsApp" content="+123456789" href="https://api.whatsapp.com/send?+123456780" />
+        </div>
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/contact/ContactOption.jsx
@@ -0,0 +1,12 @@
+const ContactOption = ({ icon, title, content, href }) => {
+    return (
+        <article className="contact__option">
+            {icon}
+            <h4>{title}</h4>
+            <h5>{content}</h5>
+            <a href={href} target="_blank">Send a message</a>
+        </article>
+    )
+}
+
+export default ContactOption
