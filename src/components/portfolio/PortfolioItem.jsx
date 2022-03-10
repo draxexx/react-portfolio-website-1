18f0d754dcebbb8a484b430cc736ff7618f546f5
Burak <burak.yilmaz@simdatix.com>
2022-03-10 10:23:41 +0300

    add CSS to Portfolio Component

--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,12 +1,16 @@
+import "./portfolio.css";
+
 const PortfolioItem = ({ title, image }) => {
     return (
-        <article className="portfolie__item">
+        <article className="portfolio__item">
             <div className="portfolio__item-image">
                 <img src={image} alt="" />
             </div>
             <h3>{title}</h3>
-            <a href="https://github.com" className="btn" target="_blank">Github</a>
-            <a href="https://github.com" className="btn btn-primary" target="_blank">Live Demo</a>
+            <div className="portfolio__item-cta">
+                <a href="https://github.com" className="btn" target="_blank">Github</a>
+                <a href="https://github.com" className="btn btn-primary" target="_blank">Live Demo</a>
+            </div>
         </article>
     )
 }
